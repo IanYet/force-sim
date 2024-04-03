5aaa6e31d2b83da6e86f31d58d811fbdbf14a622
IanYet <yt2015.97@gmail.com>
2024-04-03 18:12:54 +0800

    feat: generate edges

--- src/data/index.ts
+++ src/data/index.ts
@@ -41,9 +41,29 @@ export function geneData(vertexNum: number, edgeNum: number): Graph {
 		})
 	}
 
-	// const edgeSet: Set<string> = new Set()
+	const edgeSet: Set<string> = new Set()
 	//generate edges
-	while (edgeNum > 0) {}
+	for (let i = 0; i < edgeNum; ++i) {
+		let [start, end] = [
+			Math.floor(Math.random() * edgeNum),
+			Math.floor(Math.random() * edgeNum),
+		]
+
+		while (edgeSet.has(`${start}-${end}`) || edgeSet.has(`${end}-${start}`)) {
+			;[start, end] = [
+				Math.floor(Math.random() * edgeNum),
+				Math.floor(Math.random() * edgeNum),
+			]
+		}
+
+		edgeSet.add(`${start}-${end}`)
+
+		graphData.edges.push({
+			id: uuid(),
+			source: graphData.vertices[start].id,
+			target: graphData.vertices[end].id,
+		})
+	}
 
 	console.log(JSON.stringify(graphData))
 	return graphData
